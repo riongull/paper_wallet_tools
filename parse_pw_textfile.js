863070e395242b44f978239cab31e0e90e209bf9
Rion Gull <riongull@gmail.com>
2017-09-18 23:38:33 -0600

    initial commit - code works, but may have some bug in edge cases

---
+++ parse_pw_textfile.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+require('dotenv').config()
+const axios = require('axios');
+let json2csv = require('json2csv');
+
+const inputFile = 'paper.dash.org.txt';
+const BcAPIToken = process.env.BC_API_TOKEN;
+const BcAPI = `https://api.blockcypher.com/v1/dash/main/addrs?token=${BcAPIToken}`;
+const outputFile = 'wallet_balances.csv';
+
+function readFile(inputFile) {
+    return fs.readFileSync(inputFile, 'utf8');
+};
+
+const contents = readFile(inputFile);
+const fileObj = contents
+    .replace(/\"/g,"")
+    .replace(/ /g,"")
+    .trim()
+    .split('\n')
+    .slice(1)
+    .map(e => e.split(','))
+    .map(e => e.reduce(pubPrivs => {
+        pubPrivs["pub"] = e[0];
+        pubPrivs["priv"] = e[1];
+        return pubPrivs
+    },{}));
+    
+const addresses = fileObj.map(e => e['pub']);
+
+const getBalance = (addr) => {
+    return axios.get(`${BcAPI}/${addr}/${BcAPIToken}`)
+};
+
+const getBalances = () => {
+    return addresses.map(addr => {
+        return getBalance(addr).then(res => {
+            return res.data.balance
+        });
+    });
+};
+
+const buildBalanceObj = (balances) => {
+    return balances.map((b,i) => {
+        const o = {};
+        o.id = i+1;
+        o.address = addresses[i];
+        o.balance = b;
+        return o
+    })
+}
+
+const formatData = (data) => {
+    let fields = ['id', 'address', 'balance'];
+    let csv = json2csv({ data, fields });
+    return csv;
+}
+
+const outputData = (csv) => {
+    fs.writeFile(outputFile, csv, (err) => {
+        if (err) throw err;
+        else console.log('file saved');
+    });
+};
+
+const kickoff = async () => {
+    try {
+        const balances = await axios.all(getBalances())
+        const balanceObj = await buildBalanceObj(balances);
+        const csv = formatData(balanceObj);
+        outputData(csv);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+kickoff();
